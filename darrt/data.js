f4b77923c98e97e5b5079c815956a20da6f60adf
German Afanador <gafanador@bloomberg.net>
2020-02-24 10:03:56 -0500

    First commit

---
+++ darrt/data.js
@@ -0,0 +1,29 @@
+// ****************************************
+// DARRT framework
+// data elements 
+// properties, requireds, and enums
+// 2020-02-01 : mamund
+// ****************************************
+
+
+// this service's message properties
+// you MUST include id, dateCreated, and dateUpdated
+exports.props = [
+  'id',
+  'givenName',
+  'familyName',
+  'telephone',
+  'email',
+  'status',
+  'dateCreated',
+  'dateUpdated'
+];
+
+// required properties
+exports.reqd = ['id','email','status'];
+
+// enumerated properties
+exports.enums = [
+  {status:['pending','active','suspended','closed']}
+];
+
